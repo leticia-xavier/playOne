47692c5060d151fd9dd83f2e2dc8d98b203440d3
leticia-xavier <leticia.xavier12@hotmail.com>
2017-06-02 14:03:33 -0300

    upload

---
+++ js/myplaylist.js
@@ -0,0 +1,62 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        title:'18',
+        artist:'One Direction',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:33',
+        cover:'mix/09.jpg'
+    },
+
+    {
+        mp3:'mix/2.mp3',
+        title:'Fireproof',
+        artist:'One Direction',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'2:53',
+        cover:'mix/09.jpg'
+    },
+    {
+        mp3:'mix/3.mp3',
+        title:'Night Changes',
+        artist:'One Direction',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:00',
+        cover:'mix/09.jpg'
+    },
+        {
+        mp3:'mix/4.mp3',
+        title:'No control',
+        artist:'One Direction',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:21',
+        cover:'mix/09.jpg'
+    },
+        {
+        mp3:'mix/5.mp3',
+        title:'Steal my girl',
+        artist:'One Direction',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'5:18',
+        cover:'mix/09.jpg'
+    },
+];
